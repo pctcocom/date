9e81e5006d3f9157b508c66c2b9a007a53f63c95
pctcocom <pctco.com@gmail.com>
2020-08-10 16:19:27 +0800

    Query Add function

--- src/Query.php
+++ src/Query.php
@@ -58,4 +58,81 @@ class Query{
       //获取数字对应的星期
       return $week[$wk];
    }
+   /**
+   * @name countdown
+   * @describe 倒计时
+   * @param mixed $stamp 时间戳
+   * @return Array
+   **/
+   public static function countdown($stamp){
+      $second = $stamp - time();
+      $day = floor($second/(3600*24));
+      $second = $second%(3600*24);//除去整天之后剩余的时间
+
+      $hour = floor($second/3600);
+      $second = $second%3600;//除去整小时之后剩余的时间
+
+      $minute = floor($second/60);
+      $second = $second%60;//除去整分钟之后剩余的时间
+      //返回字符串
+      $day = $day > 9 ? $day : '0'.$day;
+      $hour = $hour > 9 ? $hour : '0'.$hour;
+      $minute = $minute > 9 ? $minute : '0'.$minute;
+      $second = $second > 9 ? $second : '0'.$second;
+      return [$day,$hour,$minute,$second];
+   }
+   /**
+   * @name interval
+   * @describe 间隔 发布文章等日期计算
+   * @param mixed $time 时间戳
+   * @param mixed $redata 返回时间类型  day = 天  hour = 小时
+   * @param mixed $format 时间格式
+   * @return
+   **/
+   public static function interval($time){
+      $limit = time() - $time;
+      $r = "";
+      if($limit < 60) {
+         $r = '刚刚发表';
+      } elseif($limit >= 60 && $limit < 3600) {
+         $r = floor($limit / 60) . '分钟前';
+      } elseif($limit >= 3600 && $limit < 86400) {
+         $r = floor($limit / 3600) . '小时前';
+      } elseif($limit >= 86400 && $limit < 2592000) {
+         $r = floor($limit / 86400) . '天前';
+      } elseif($limit >= 2592000 && $limit < 31104000) {
+         $r = floor($limit / 2592000) . '个月前';
+      } else {
+         $r = date('Y-m-d',$time);
+      }
+      return $r;
+   }
+   /**
+   * @name get day
+   * @describe 获取上下天数
+   * @param mixed $day 获取天数
+   * @param mixed $order 排序 asc/desc
+   * @param mixed $time 开始时间 时间戳
+   * @param mixed $format 时间格式
+   * @return Array
+   **/
+   public static function day($day,$time,$order = 'asc',$format = 'Y-m-d'){
+      $week = $order == 'asc'?date('w', $time):$day;
+      $date = [];
+      for ($i=1; $i<=$day; $i++){
+         $date[$i] = date($format ,strtotime( '+' . $i-$week .' days', $time));
+      }
+      return $date;
+   }
+   /**
+   * @name time_17
+   * @describe 17位时间戳
+   * @return String
+   **/
+   public static function time_17(){
+      list($usec, $sec) = explode(" ", microtime());
+      $millisecond = round($usec*1000);
+      $millisecond = str_pad($millisecond,3,'0',STR_PAD_RIGHT);
+      return strval(date("YmdHis").$millisecond);
+   }
 }
